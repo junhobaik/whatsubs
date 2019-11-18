e530888c77ee9137d6363d4fea204e0a64ae18ca
Junho Baik <junhobaik@gmail.com>
2019-11-18 22:35:58 +0900

    icon img 추가

--- src/List/img/index.js
+++ src/List/img/index.js
@@ -1,4 +1,6 @@
 export default {
   bugs: require("./bugs.png"),
-  melon: require("./melon.png")
+  melon: require("./melon.png"),
+  bear: require("./bear.png"),
+  coupang: require("./coupang.png")
 };
