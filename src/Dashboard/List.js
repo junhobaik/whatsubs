d625e86d2b846e1674cfe3d7370c701513c7d7bf
Junho Baik <junhobaik@gmail.com>
2019-11-21 00:26:35 +0900

    Dashboard, List 마크업 수정

--- src/Dashboard/List.js
+++ src/Dashboard/List.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
+import { FontAwesomeIcon as Fa } from "@fortawesome/react-native-fontawesome";
+import {
+  faDollarSign,
+  faYenSign,
+  faWonSign,
+  faCalendarDay,
+  faCalendarAlt
+} from "@fortawesome/free-solid-svg-icons";
+
 import addListData from "../AddList/list";
 
 const List = ({ list }) => {
@@ -30,19 +39,78 @@ const List = ({ list }) => {
       }
     };
 
+    const makeCurrencyIcon = currency => {
+      let icon;
+      switch (currency) {
+        case "dollar":
+          icon = faDollarSign;
+          break;
+        case "won":
+          icon = faWonSign;
+          break;
+        case "yen":
+          icon = faYenSign;
+          break;
+        default:
+          icon = faDollarSign;
+          break;
+      }
+
+      return <Fa icon={icon} style={{ color: "#ddd" }} size={14} />;
+    };
+
     const makedIcon = makeIcon(v.icon.type);
+    const makedCurrencyIcon = makeCurrencyIcon(v.currency);
 
     return (
       <TouchableOpacity
         key={v.id}
-        style={{ backgroundColor: "#222", marginBottom: 10, borderRadius: 7 }}
+        style={{
+          backgroundColor: "rgb(26, 27, 29)",
+          marginBottom: 10,
+          borderRadius: 7,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          padding: 10
+        }}
       >
         <View>
-          <Text
-            style={{ color: "#ddd" }}
-          >{`${splitedDate[1]}.${splitedDate[2]}`}</Text>
-          <Text style={{ color: "white" }}>{v.title}</Text>
-          <View>{makedIcon}</View>
+          <Text style={{ color: "#fff", fontSize: 18, fontWeight: "bold" }}>
+            {v.title}
+          </Text>
+          <View style={{ marginTop: 10 }}>{makedIcon}</View>
+        </View>
+        <View
+          style={{ alignItems: "flex-end", justifyContent: "space-between" }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <View style={{ marginRight: 2 }}>{makedCurrencyIcon}</View>
+            <Text style={{ color: "#fff", fontSize: 16 }}>{v.price}</Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Text
+              style={{ color: "#bbb", fontSize: 16 }}
+            >{`${splitedDate[1]}.${splitedDate[2]}`}</Text>
+            <View
+              style={{
+                backgroundColor:
+                  v.period === "m" ? "rgb(4, 132, 255)" : "rgb(252, 71, 59)",
+                borderRadius: "100%",
+                height: 16,
+                width: 16,
+                alignItems: "center",
+                justifyContent: "center",
+                marginLeft: 3,
+                opacity: 0.7
+              }}
+            >
+              <Fa
+                icon={v.period === "m" ? faCalendarDay : faCalendarAlt}
+                style={{ color: "#fff", margin: 0 }}
+                size={10}
+              />
+            </View>
+          </View>
         </View>
       </TouchableOpacity>
     );
