386d56dd6715f3e57217e9570cc69e5787745270
Junho Baik <junhobaik@gmail.com>
2019-11-26 22:58:46 +0900

    img 추가

--- src/AddList/img/index.js
+++ src/AddList/img/index.js
@@ -3,5 +3,16 @@ export default {
   melon: require("./melon.png"),
   bear: require("./bear.png"),
   coupang: require("./coupang.png"),
-  setapp: require("./setapp.png")
+  setapp: require("./setapp.png"),
+  amazonprime: require("./amazon_prime.png"),
+  ulysses: require("./ulysses.png"),
+  genie: require("./genie.png"),
+  laftel: require("./laftel.png"),
+  watcha: require("./watcha.png"),
+  millie: require("./millie.png"),
+  office365: require("./office365.png"),
+  googleone: require("./googleone.png"),
+  wavve: require("./wavve.png"),
+  vibe: require('./vibe.png'),
+  flo: require('./flo.png')
 };
